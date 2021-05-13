245f2210921609fea017ac4404bae693dbf38aa4
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-05-12 21:09:26 -0500

    semantic

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,12 +3,39 @@ import { connect } from "react-redux";
 
 
 class SongList extends Component {
+    renderList(){
+       return this.props.songs.map( (song)=> {
+           return(
+               <div className={"item"} key={song.title}>
+                   <div className={"right floated content"}>
+                       <button className={"ui button primary"}>
+                           Select
+                       </button>
+                   </div>
+
+                   <div className={"content"}>{song.title}</div>
+               </div>
+           )
+       })
+    }
+
+
+
+
+
     render() {
+        console.log(this.props)
         return (
-            <div> Song List</div>
+            //calls the helper method above
+            <div className={"ui divided list"}>{this.renderList()}</div>
         );
     }
 }
-
-export default connect()(SongList);
+//always define this to get props from store
+const mapStateToProps = (state) => {
+    console.log(state)
+    return { songs: state.songs }
+}
+//always define this
+export default connect(mapStateToProps)(SongList);
 
