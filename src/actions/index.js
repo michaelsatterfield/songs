195cc8cfda106af6502be74e0af9d4ef471ee2ea
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-05-18 15:28:54 -0500

    create song detail functional componenent

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,4 +7,4 @@ export const selectSong = song => {
     }
 };
 
-export
+
---
+++ src/components/SongDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from "react-redux";
+
+const SongDetail = () => {
+    return (
+        <div>
+            Song Detail
+        </div>
+    );
+};
+
+export default SongDetail;
